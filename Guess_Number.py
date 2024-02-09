d46798c20d9bd494b9947f9881c1f05783d88554
RyanJohnKennedy <Ryan.Kennedy.John@gmail.com>
2024-02-09 15:51:27 +0200

    Computer Guesses

--- Guess_Number.py
+++ Guess_Number.py
@@ -16,4 +16,27 @@ def guess(x):
     print(f"Yay, congrats! You guessed the number ({random_number}) in {counter} tries.")
 
 
-guess(10)
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    counter = 0
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else :
+            guess = low
+
+        feedback = input(f"Is {guess} too high (H), too low (L), or correct (C)?: ").lower()
+
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+
+        counter += 1
+
+    print(f"Yay! The computer your number ({guess}) in {counter} tries.")
+
+maxNumber = int(input("Enter max number for the computer to guess: "))
+computer_guess(maxNumber)
